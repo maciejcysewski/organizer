6bf5b54911ffdf2ec49e60b19bb6d2173e0511e0
Maciej Cysewski <maciej@jcode.pl>
2016-09-24 10:27:27 +0200

    finish method showEventList and ListView

--- src/main/java/pl/jcode/organizer/controller/MainWindowController.java
+++ src/main/java/pl/jcode/organizer/controller/MainWindowController.java
@@ -1,8 +1,16 @@
 package pl.jcode.organizer.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import pl.jcode.organizer.Main;
 import pl.jcode.organizer.domain.Event;
@@ -25,10 +33,18 @@ public class MainWindowController {
 	@FXML
 	private TextField descField;
 
+	@FXML
+	private ListView<String> listView;
+
+	public void initialize() {
+		listView.setId(null);
+	}
+
 	public void getDatePicker() {
 		try {
 			newDate = datePicker.getValue().toString();
 			System.out.println(newDate);
+			showEventList(newDate);
 		} catch (Exception e) {
 			newDate = "";
 			System.out.println("Wyst¹pi³ wyj¹tek (data)\n" + e);
@@ -68,17 +84,29 @@ public class MainWindowController {
 					Main.entityManager.getTransaction().commit();
 
 					// show result on ListView
-					// showEventList(newDate);
+					showEventList(newDate);
 
 					// clear all fields
-					hourField.setText(null);
-					descField.setText(null);
-					newHour = null;
-					newDesc = null;
+					hourField.setText("");
+					descField.setText("");
+					newHour = "";
+					newDesc = "";
 				}
 			}
 		}
 
+	}
+
+	public void showEventList(String date) {
+		TypedQuery<Event> query = Main.entityManager.createQuery("SELECT e FROM Event e WHERE e.date = :date order by e.hour ASC", Event.class);
+		query.setParameter("date", date);
+		List<Event> events = query.getResultList();
+		List<String> eventS = new ArrayList<>();
+		for (Event event : events) {
+			eventS.add(event.getHour() + ", " + event.getEvent());
+		}
+		ObservableList<String> items = FXCollections.observableArrayList(eventS);
+		listView.setItems(items);
 	}
 
 }
